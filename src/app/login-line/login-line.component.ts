f0f20015760e037fea2fcd0f55fd1b3b201f5cf8
abg74123 <bisza_i.d@hotmail.com>
2024-01-16 16:32:53 +0700

    update

--- src/app/login-line/login-line.component.ts
+++ src/app/login-line/login-line.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import liff from "@line/liff";
 
 @Component({
   selector: 'app-login-line',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginLineComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    liff.init({liffId:'2002624343-g6braWW3'})
+    if(liff.isLoggedIn()){
+      const profile = liff.getProfile()
+      console.log("profile => ",profile)
+    }else{
+      liff.login()
+    }
   }
 
+  login(){
+
+    this.http.get(' https://api.line.me/oauth2/v2.1/authorize').subscribe((res:any) => console.log("login => ",res))
+
+  }
 }
--- src/app/login-line/login-line.module.ts
+++ src/app/login-line/login-line.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LoginLineRoutingModule } from './login-line-routing.module';
 import { LoginLineComponent } from './login-line.component';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
   declarations: [LoginLineComponent],
   imports: [
     CommonModule,
-    LoginLineRoutingModule
+    LoginLineRoutingModule,
+    HttpClientModule
   ]
 })
 export class LoginLineModule { }
