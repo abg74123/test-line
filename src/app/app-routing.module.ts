c1f9278d3b0bca8a5d2fdcd1f59db5941a6d2525
abg74123 <bisza_i.d@hotmail.com>
2024-01-16 16:51:27 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import {ChartComponent} from "./connect-line/chart/chart.component";
 
 const routes: Routes = [
   {
+    path:'',redirectTo:'login',pathMatch:'full'
+  },
+  {
     path: 'connect',
     loadChildren: () => import('./connect-line/connect-line.module').then(m => m.ConnectLineModule)
   },
--- src/app/login-line/login-line.component.ts
+++ src/app/login-line/login-line.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import liff from "@line/liff";
 
+liff.init({liffId:'2002624343-g6braWW3',withLoginOnExternalBrowser: true})
+
 @Component({
   selector: 'app-login-line',
   templateUrl: './login-line.component.html',
   styleUrls: ['./login-line.component.css']
 })
+
 export class LoginLineComponent implements OnInit {
 
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
-    liff.init({liffId:'2002624343-g6braWW3'})
     if(liff.isLoggedIn()){
       const profile = liff.getProfile()
       console.log("profile => ",profile)
