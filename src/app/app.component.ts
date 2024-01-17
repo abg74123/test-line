a8cfaa4228b4def7eb6c5e4c0f8cc7fa3168fec3
abg74123 <bisza_i.d@hotmail.com>
2024-01-17 17:00:35 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,28 @@ import {Component, OnInit} from '@angular/core';
 import liff from "@line/liff";
 import {BehaviorSubject} from "rxjs";
 
-    liff.init({liffId:'2002624343-g6braWW3',withLoginOnExternalBrowser: true})
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  profile$:any = new BehaviorSubject({})
+export class AppComponent implements OnInit {
+  profile$: any = new BehaviorSubject({})
 
-ngOnInit(): void {
-
-
-    if(liff.isLoggedIn()){
+  ngOnInit(): void {
+    liff.init({liffId: '2002624343-g6braWW3', withLoginOnExternalBrowser: true}).then(async ()=>{
+      if (liff.isLoggedIn()) {
       console.log("---login success---")
       const profile = liff.getProfile()
-      console.log("profile => ",profile)
+      console.log("profile => ", profile)
       this.profile$.next(profile)
-    }else{
+    } else {
       liff.login()
     }
+    })
   }
 
-  logout(){
+  logout() {
     liff.logout()
   }
 }
