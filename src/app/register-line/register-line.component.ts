4775498e1cc60079c36ad87ca5a22388f3d54c16
abg74123 <bisza_i.d@hotmail.com>
2024-02-21 12:11:57 +0700

    update

--- src/app/register-line/register-line.component.ts
+++ src/app/register-line/register-line.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import liff from "@line/liff";
-import {BehaviorSubject, concat, map} from "rxjs";
+import {concat, map} from "rxjs";
 import {Router} from "@angular/router";
 import {environment} from "../core/environment.prod";
 import {LineService} from "../core/line.service";
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-line',
@@ -16,81 +16,70 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 export class RegisterLineComponent implements OnInit {
   loading = true
 
-  formRegister = this.fb.group({
-    blockedNote: "",
-    compId: "",
-    customerCode: "",
-    customerCompanyContactInfo: undefined,
-    customerContactInfo: this.fb.group({
-      address: undefined,
-      emails: this.fb.array(['']),
-      firstName: "",
-      fullName: ['', Validators.required] ,
-      lastName: ['', Validators.required] ,
-      mobiles: this.fb.array(['']),
-      note: "",
-      personPic: "",
-      prefixName: ['', Validators.required],
-      shippingAddress: [],
-      social: undefined,
-      tags: [],
-      taxNo: ""
-    }),
-    customerNote: "",
-    isBlocked: false,
-    isMember: false,
-    orderType: undefined,
-    socialContact: [],
-    tags: [],
-    wasVendor: false
+  formRegister: any = this.fb.group({
+    address: "",
+    emails: this.fb.array(['']),
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    mobiles: this.fb.array(['']),
+    personPic: "",
+    prefixName: ['', Validators.required],
   })
 
 
   constructor(private fb: FormBuilder, private route: Router, private lineService: LineService) {
   }
 
-  ngOnInit(): void {
-    liff.init({liffId: environment.liffId, withLoginOnExternalBrowser: true}).then(async () => {
-      if (liff.isLoggedIn()) {
-        const profile = await liff.getProfile()
-        console.log("profile => ", profile)
+  async ngOnInit() {
+    await liff.init({liffId: environment.liffId, withLoginOnExternalBrowser: true})
+    if (liff.isLoggedIn()) {
 
-        const getMemberDetail$ = this.lineService.getMemberDetail(profile.userId).pipe(
+      // * Get Profile Line
+      const profile = await liff.getProfile()
+      console.log("profile => ", profile)
+
+      // * patch value image
+      this.formRegister.get('personPic').patchValue(profile.pictureUrl)
+
+      /**
+       * Get Member Fount:
+       * if true => Change Rich Menu And Redirect To ...
+       * if false => return Erro (end)
+       */
+
+        // ^ Get Member Detail
+      const getMemberDetail$ = this.lineService.getMemberDetail(profile.userId).pipe(
           map(member => {
             if (!member) {
               throw new Error("member not fount");
             }
           })
         )
-        const changeRichMenu$ = this.lineService.changeRichMenu(profile.userId)
+      // ^ Change Rich Menu
+      const changeRichMenu$ = this.lineService.changeRichMenu(profile.userId)
 
-        concat(getMemberDetail$, changeRichMenu$).subscribe(
-          {
-            error: (err) => {
-              this.loading = false;
-              console.error(err)
-            },
-            complete: () => {
-              // this.route.navigate(['/info'])
-              window.location.href = 'https://dev-slip.gapp-biz.com/'
-            }
+      concat(getMemberDetail$, changeRichMenu$).subscribe(
+        {
+          error: (err) => {
+            this.loading = false;
+            console.error(err)
+          },
+          complete: () => {
+            // this.route.navigate(['/info'])
+            window.location.href = 'https://dev-slip.gapp-biz.com/'
           }
-        )
+        }
+      )
 
-      } else {
-        liff.login()
-      }
-    })
-  }
-
-  get formCustomerContactInfo() {
-    return this.formRegister.get("customerContactInfo") as any
+    } else {
+      liff.login()
+    }
   }
 
   async register() {
     console.log('register')
-    console.log("formRegister valid => ",this.formRegister.valid)
-    // if (this.formRegister.valid) {
+    console.log("formRegister valid => ", this.formRegister.valid)
+    if (this.formRegister.valid) {
       const profile = await liff.getProfile()
       const body = this.formRegister.value
       console.log({body})
@@ -105,11 +94,11 @@ export class RegisterLineComponent implements OnInit {
           }
         }
       )
-    // }else{
-    //  this.formCustomerContactInfo.get('prefixName').markAsTouched()
-    //  this.formCustomerContactInfo.get('firstName').markAsTouched()
-    //  this.formCustomerContactInfo.get('lastName').markAsTouched()
-    // }
+    } else {
+      this.formRegister.get('prefixName').markAsTouched()
+      this.formRegister.get('firstName').markAsTouched()
+      this.formRegister.get('lastName').markAsTouched()
+    }
   }
 
 }
